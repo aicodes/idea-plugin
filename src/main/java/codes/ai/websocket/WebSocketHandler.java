9fefdb03889fc0d91206caa78e2401238297cd4e
Eric Xu <exu@ai.codes>
2016-09-24 17:56:27 -0700

    adjust the plugin

--- src/main/java/codes/ai/websocket/WebSocketHandler.java
+++ src/main/java/codes/ai/websocket/WebSocketHandler.java
@@ -17,81 +17,81 @@ import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author xuy.
- *         Copyright (c) Ai.codes
- */
+/** @author xuy. Copyright (c) Ai.codes */
 
 /// Not used for now, as it is for reading messages from server.
-	@ChannelHandler.Sharable
+@ChannelHandler.Sharable
 public class WebSocketHandler extends SimpleChannelInboundHandler<Object> {
-	private static final Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
+  private static final Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
 
-	private final WebSocketClientHandshaker handshaker;
-	private ChannelPromise handshakeFuture;
+  private final WebSocketClientHandshaker handshaker;
+  private ChannelPromise handshakeFuture;
 
-	public WebSocketHandler(final WebSocketClientHandshaker handshaker) {
-		this.handshaker = handshaker;
-	}
+  public WebSocketHandler(final WebSocketClientHandshaker handshaker) {
+    this.handshaker = handshaker;
+  }
 
-	public ChannelFuture handshakeFuture() {
-		return handshakeFuture;
-	}
+  public ChannelFuture handshakeFuture() {
+    return handshakeFuture;
+  }
 
-	@Override
-	public void handlerAdded(final ChannelHandlerContext ctx) throws Exception {
-		handshakeFuture = ctx.newPromise();
-	}
+  @Override
+  public void handlerAdded(final ChannelHandlerContext ctx) throws Exception {
+    handshakeFuture = ctx.newPromise();
+  }
 
-	@Override
-	public void channelActive(final ChannelHandlerContext ctx) throws Exception {
-		handshaker.handshake(ctx.channel());
-	}
+  @Override
+  public void channelActive(final ChannelHandlerContext ctx) throws Exception {
+    handshaker.handshake(ctx.channel());
+  }
 
-	@Override
-	public void channelInactive(final ChannelHandlerContext ctx) throws Exception {
-		System.out.println("WebSocket WsClient disconnected!");
-	}
+  @Override
+  public void channelInactive(final ChannelHandlerContext ctx) throws Exception {
+    System.out.println("WebSocket WsClient disconnected!");
+  }
 
-	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-		final Channel ch = ctx.channel();
-		if (!handshaker.isHandshakeComplete()) {
-			System.out.println("Instance connected");
-			handshaker.finishHandshake(ch, (FullHttpResponse) msg);
-			handshakeFuture.setSuccess();
-			return;
-		}
+  @Override
+  protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+    final Channel ch = ctx.channel();
+    if (!handshaker.isHandshakeComplete()) {
+      System.out.println("Instance connected");
+      handshaker.finishHandshake(ch, (FullHttpResponse) msg);
+      handshakeFuture.setSuccess();
+      return;
+    }
 
-		if (msg instanceof FullHttpResponse) {
-			final FullHttpResponse response = (FullHttpResponse) msg;
-			throw new Exception("Unexpected FullHttpResponse (getStatus=" + response.getStatus() + ", content="
-					+ response.content().toString(CharsetUtil.UTF_8) + ')');
-		}
+    if (msg instanceof FullHttpResponse) {
+      final FullHttpResponse response = (FullHttpResponse) msg;
+      throw new Exception(
+          "Unexpected FullHttpResponse (getStatus="
+              + response.status()
+              + ", content="
+              + response.content().toString(CharsetUtil.UTF_8)
+              + ')');
+    }
 
-		final WebSocketFrame frame = (WebSocketFrame) msg;
-		if (frame instanceof TextWebSocketFrame) {
-			final TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
-			// uncomment to print request
-			// logger.info(textFrame.text());
-		} else if (frame instanceof PongWebSocketFrame) {
-		} else if (frame instanceof CloseWebSocketFrame)
-			ch.close();
-		else if (frame instanceof BinaryWebSocketFrame) {
-			// uncomment to print request
-			// logger.info(frame.content().toString());
-		}
+    final WebSocketFrame frame = (WebSocketFrame) msg;
+    if (frame instanceof TextWebSocketFrame) {
+      final TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
+      // uncomment to print request
+      // logger.info(textFrame.text());
+    } else if (frame instanceof PongWebSocketFrame) {
+    } else if (frame instanceof CloseWebSocketFrame) ch.close();
+    else if (frame instanceof BinaryWebSocketFrame) {
+      // uncomment to print request
+      // logger.info(frame.content().toString());
+    }
+  }
 
-	}
+  @Override
+  public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause)
+      throws Exception {
+    cause.printStackTrace();
 
-	@Override
-	public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) throws Exception {
-		cause.printStackTrace();
+    if (!handshakeFuture.isDone()) {
+      handshakeFuture.setFailure(cause);
+    }
 
-		if (!handshakeFuture.isDone()) {
-			handshakeFuture.setFailure(cause);
-		}
-
-		ctx.close();
-	}
+    ctx.close();
+  }
 }
